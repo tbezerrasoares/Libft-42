a985ef520474b26d297fee0c8851d9ad8cb35a43
tbezerrasoares <tbezerrasoares@gmail.com>
2023-10-04 15:32:23 +0100

    Files the libft

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbezerra <tbezerra@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 14:29:53 by tbezerra          #+#    #+#             */
+/*   Updated: 2023/10/04 12:59:03 by tbezerra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+int	ft_isalpha(int);
+int	ft_isdigit(int);
+int	ft_isalnum(int);
+int	ft_isascii(int);
+int	ft_isprint(int);
+int	ft_strlen(const char *);
+int	ft_toupper(int);
+int	ft_tolower(int);
+
+#endif
