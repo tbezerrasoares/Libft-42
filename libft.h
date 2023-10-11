6eff0b2492f34c0d6935c2019797e93fcfca572d
tbezerrasoares <tbezerrasoares@gmail.com>
2023-10-11 17:18:13 +0100

    Add files the libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tbezerra <tbezerra@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:29:53 by tbezerra          #+#    #+#             */
-/*   Updated: 2023/10/09 15:33:43 by tbezerra         ###   ########.fr       */
+/*   Updated: 2023/10/11 10:32:27 by tbezerra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ size_t	ft_strlen(const char *);
 int	ft_toupper(int);
 int	ft_tolower(int);
 int	ft_strncmp(char *s1, char *s2, unsigned int n);
+int atoi(const char *);
 
 #endif
